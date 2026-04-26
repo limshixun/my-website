aaa9713f825c9735d041b4906a529e5c654af5af
limshixun <limshixun1106@gmail.com>
2026-04-26 16:19:58 +0800

    feat: handle terminal focus state & move terminal container into Terminal

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,7 @@ export default function Home() {
 	return (
 		<div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
 			<main className="flex flex-1 w-full max-w-5xl flex-col items-center justify-between py-32  bg-white dark:bg-black sm:items-start">
-				<div className="bg-zinc-500/30 overflow-hidden border border-zinc-700 backdrop-blur-2xl w-full h-full grid grid-rows-[1fr_auto_1fr] rounded-xl">
-					<div className="p-2 bg-zinc-950/90 text-zinc-400 lowercase text-center">Lim Shi Xun's Portfolio</div>
-					<div className="p-2 border-y border-zinc-700 min-h-120 ">
-						<Terminal />
-					</div>
-					<div className="p-2 bg-zinc-950/90" >footer</div>
-				</div>
+				<Terminal />
 			</main>
 		</div>
 	);
--- components/Terminal.tsx
+++ components/Terminal.tsx
@@ -1,12 +1,29 @@
 "use client"
+import { useState, useRef, useEffect } from "react"
 export function Terminal() {
+	const [input, setInput] = useState("")
+
+	useEffect(() => {
+		const terminal = document.getElementById("terminal")!
+		console.log(terminal)
+		terminal.addEventListener("click", () => {
+			terminal.focus()
+		})
+		return () => terminal.removeEventListener("click", () => terminal.focus())
+	}, [])
+
 	return (
-		<div className="flex">
-			<span className="font-mono">limshixun@portfolio $ </span>
-			<div>
-
+		<div id="terminal" tabIndex={1} onBlur={() => setInput("")} onFocus={() => setInput("focused")} className="bg-zinc-500/30 overflow-hidden border border-zinc-700 backdrop-blur-2xl w-full h-full grid grid-rows-[1fr_auto_1fr] rounded-xl">
+			<div className="p-2 bg-zinc-950/90 text-zinc-400 lowercase text-center">Lim Shi Xun's Portfolio</div>
+			<div className="p-2 border-y border-zinc-700 min-h-120 ">
+				<div className="flex gap-2">
+					<span className="font-mono">limshixun@portfolio $ </span>
+					<div >
+						<p className="font-mono">{input}</p>
+					</div>
+				</div>
 			</div>
-
+			<div className="p-2 bg-zinc-950/90" >footer</div>
 		</div>
 	)
 }
