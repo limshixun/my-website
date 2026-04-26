abcd4be5e7e7172f4e930e4c3c663393678d70b7
limshixun <limshixun1106@gmail.com>
2026-04-26 15:42:32 +0800

    feat: created a simple terminal look

---
+++ components/Terminal.tsx
@@ -0,0 +1,12 @@
+"use client"
+export function Terminal() {
+	return (
+		<div className="flex">
+			<span className="font-mono">limshixun@portfolio $ </span>
+			<div>
+
+			</div>
+
+		</div>
+	)
+}
