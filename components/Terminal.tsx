62186c3dde2c971d4aac33cbbfca80d4d81b912a
limshixun <limshixun1106@gmail.com>
2026-04-28 21:03:40 +0800

    chore: replace getElementById with useRef

--- components/Terminal.tsx
+++ components/Terminal.tsx
@@ -1,30 +1,32 @@
 "use client"
-import { useState, useEffect } from "react"
+import { useState, useEffect, useRef } from "react"
 import { tokenize } from "@/components/Parser"
 export function Terminal() {
+	const terminalRef = useRef<HTMLDivElement>(null)
+	const inputRef = useRef<HTMLElement>(null)
 	const [input, setInput] = useState("")
 	const [logs, setLogs] = useState([{ cmd: "intro", output: "Hi, ShiXun here, type `help` to show all the available options" }])
 
 	// Focus on input when clicked on terminal
 	useEffect(() => {
-		const terminal = document.getElementById("terminal")!
-		const input = document.getElementById('input')!
-		const handleTerminalFocus = () => input.focus()
+		const handleTerminalFocus = () => inputRef.current?.focus()
 
-		terminal.addEventListener("click", handleTerminalFocus)
+		terminalRef.current?.addEventListener("click", handleTerminalFocus)
 		return () => {
-			terminal.removeEventListener("click", handleTerminalFocus)
+			terminalRef.current?.removeEventListener("click", handleTerminalFocus)
 		}
 	}, [])
 
 	const executeCommand = (command: string) => {
-		console.log(tokenize(command))
-		const trimmedCommand = command.trim();
-		if (trimmedCommand === "clear") {
+		const tokens = tokenize(command)
+		if (tokens.length === 0) return ""
+		if (command === "clear") {
 			setLogs([])
 			return ""
 		}
-		return trimmedCommand === "" ? "" : `${trimmedCommand}: command not found.`
+
+		const output = command === "" ? "" : `${command}: command not found.`
+		writeHistory(output)
 	}
 
 	const writeHistory = (output: string) => {
@@ -33,7 +35,7 @@ export function Terminal() {
 
 	return (
 		<div
-			id="terminal"
+			ref={terminalRef}
 			className="bg-zinc-500/30 font-mono overflow-hidden border border-zinc-700 backdrop-blur-2xl w-full h-full grid grid-rows-[1fr_auto_1fr] rounded-xl">
 			<div className="p-2 bg-zinc-950/90 text-zinc-400 lowercase text-center">LimShiXun's Portfolio</div>
 			<div className="p-1 border-y border-zinc-700 min-h-120 ">
@@ -52,7 +54,7 @@ export function Terminal() {
 					<div className="flex gap-2">
 						<span>limshixun@portfolio$</span>
 						<span
-							id="input"
+							ref={inputRef}
 							contentEditable
 							onInput={(e) => {
 								setInput(e.currentTarget.textContent || "")
@@ -61,8 +63,7 @@ export function Terminal() {
 								if (e.code.toLowerCase() === "enter") {
 									// Prevent to nextline
 									e.preventDefault()
-									const output = executeCommand(e.currentTarget.textContent)
-									writeHistory(output)
+									executeCommand(e.currentTarget.textContent)
 									// Reset 
 									e.currentTarget.textContent = ""
 									setInput("")
